ff92c3c554f1aca352debdc2c477bd5d78239794
Diego <michal.klimczewski@gmail.com>
2022-10-29 12:53:12 +0200

    doddanie typów templatek

--- src/utils/initAtoms.ts
+++ src/utils/initAtoms.ts
@@ -2,4 +2,7 @@ import {atomWithStorage} from 'jotai/utils';
 
 export const redirectAtom = atomWithStorage('redirect', false);
 export const multiVerseAtom = atomWithStorage('multiVerse', false);
-export const template = atomWithStorage('template', '');
+
+type templateType = 'markdown' | 'text' | 'link';
+
+export const template = atomWithStorage<templateType>('template', 'text');
